eaa0db84b507449cbef8cd6a995129dba11bc4a6
dorony <doronyaakobov@gmail.com>
2023-05-04 00:12:11 +0300

    Re-defined Patient model + added to site. UI minor changes.

--- patient_view/management/commands/create_fake_fatients.py
+++ patient_view/management/commands/create_fake_fatients.py
@@ -15,19 +15,22 @@ class Command(BaseCommand):
 
     def handle(self, n, *args, **options):
         for i in range(n):
-            first_name = faker.first_name()
-            last_name = faker.last_name()
-            full_name = f"{last_name} {first_name}"
-            start_enter_date = datetime.strptime('30.11.1997', '%d.%m.%Y')
-            end_enter_date = datetime.strptime('30.11.1998', '%d.%m.%Y')
-            start_exit_date = datetime.strptime('10.12.1998', '%d.%m.%Y')
-            end_exit_date = datetime.strptime('10.12.1999', '%d.%m.%Y')
+            # start_enter_date = datetime.strptime('30.11.1997', '%d.%m.%Y')
+            # end_enter_date = datetime.strptime('30.11.1998', '%d.%m.%Y')
+            # start_exit_date = datetime.strptime('10.12.1998', '%d.%m.%Y')
+            # end_exit_date = datetime.strptime('10.12.1999', '%d.%m.%Y')
 
             Patient.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                full_name=full_name,
-                id=faker.random_number(digits=9),
-                enter_date=faker.date_between(start_date=start_enter_date, end_date=end_enter_date),
-                leave_date=faker.date_between(start_date=start_exit_date, end_date=end_exit_date)
+                # meta
+                food_cosher=faker.boolean(),
+                food_diary_restricted=faker.boolean(),
+                # patient
+                first_name=faker.first_name(),
+                last_name=faker.last_name(),
+                # full_name=f"{last_name} {first_name}",
+                patient_id=faker.random_number(digits=9),
+                # enter_date=faker.date_between(start_date=start_enter_date, end_date=end_enter_date),
+                # leave_date=faker.date_between(start_date=start_exit_date, end_date=end_exit_date)
+                description=faker.text(),
+                birth_date=faker.date_of_birth(),
             )
--- patient_view/models.py
+++ patient_view/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Patient(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=46)
     last_name = models.CharField(max_length=46)
-    full_name = models.CharField(max_length=92)
-    id = models.DecimalField(max_digits=9, decimal_places=0, primary_key=True)
-    enter_date = models.DateField()
-    leave_date = models.DateField()
+    full_name = models.CharField(max_length=93, help_text="Placeholder. Please do not edit or delete this.")
+    patient_id = models.DecimalField(max_digits=9, decimal_places=0)
+    enter_date = models.DateTimeField(default=timezone.now)
+    leave_date = models.DateTimeField(default=None, null=True, blank=True)
+    description = models.TextField(default=None, null=True, blank=True)
+    birth_date = models.DateField(default=None, null=True, blank=True)
+    food_cosher = models.BooleanField(default=None, null=True, blank=True)
+    food_diary_restricted = models.BooleanField(default=None, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+
+    def save(self, *args, **kwargs):
+        self.full_name = f"{self.last_name} {self.first_name}"
+        super().save(*args, **kwargs)
