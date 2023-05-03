93055f9668ad6fa1f6139e2965bbb1a1c862881b
dorony <doronyaakobov@gmail.com>
2023-05-04 00:25:10 +0300

    Defined admin UI.

--- patient_view/admin.py
+++ patient_view/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from . import models
+
+
+# admin view
+class PatientAdmin(admin.ModelAdmin):
+    date_hierarchy = "enter_date"
+    search_fields = (
+        "id",
+        "enter_date",
+        "patient_id",
+        "last_name",
+        "first_name",
+        "food_cosher",
+        "food_diary_restricted",
+        "birth_date",
+        "description",
+        "leave_date",
+
+    )
+    list_display = search_fields
+
+
 # Register your models here.
-admin.site.register(models.Patient)
+admin.site.register(models.Patient, PatientAdmin)
