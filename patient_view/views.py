93236946aa371556dad102b41781ab337a8e797e
dorony <doronyaakobov@gmail.com>
2023-07-31 16:38:45 +0300

    Adding Patients table WIP

--- patient_view/views.py
+++ patient_view/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from patient_view.models import Patient
+
 
 # Create your views here.
 def patients_list(request):
-    return render(request, "patient_view/patient_list.html")
+    qs = Patient.objects.all()
+    return render(request, "patient_view/patient_list.html",
+                  {
+                      "patients": qs,
+                  })
